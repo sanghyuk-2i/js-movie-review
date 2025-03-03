d1b8491392b384f428b20d97d2865a7673110934
sanghyuk-2i <desummit1@gmail.com>
2025-03-03 23:45:13 +0900

    Updates

--- assets/index-Z0SxX_1t.js
+++ assets/index-Z0SxX_1t.js
@@ -152,6 +152,10 @@ const thumbnailStore = createObserver({
   thumbnailSrc: "",
   thumbnailVoteAverage: 0
 });
+const searchStore = createObserver({
+  hasSearchValue: false,
+  searchValue: ""
+});
 const HelperLabel = (props) => {
   const { content } = props;
   return `
@@ -168,27 +172,73 @@ const Label = (props) => {
   `;
 };
 const Input = (props) => {
-  const { name, label, placeholder, helperText, required } = props;
-  const width = "width: 100%;";
+  const { name, label, placeholder, helperText, required, value } = props;
+  const _width = "width: 100%; flex: 1;";
   const _placeholder = placeholder ? `placeholder="${placeholder}"` : "";
+  const _value = value ? `value="${value}"` : "";
   return `
     <div style="display: flex; flex-direction: column; gap: 8px;">
       ${label ? Label({ name, content: label, required }) : ""}
       <input
         id=${name}
         name=${name}
-        style="${width} height: 44px; padding: 8px; border: 1px solid var(--color-white); color: var(--color-white); border-radius: 8px; font-size: 16px; background-color: var(--color-bluegray-100);"
+        style="${_width} height: 44px; padding: 8px; border: 1px solid var(--color-white); color: var(--color-white); border-radius: 8px; font-size: 16px; background-color: var(--color-bluegray-100);"
         ${_placeholder}
+        ${_value}
       />
       ${helperText ? HelperLabel({ content: helperText }) : ""}
     </div>
   `;
 };
+const searchParams = {
+  get: (key) => {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(key);
+  },
+  set: (key, value, replace = false) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set(key, value);
+    const newUrl = `${window.location.pathname}?${params.toString()}`;
+    if (replace) {
+      window.history.replaceState({}, "", newUrl);
+    } else {
+      window.history.pushState({}, "", newUrl);
+    }
+  }
+};
 const SearchInput = () => {
-  return ` 
-    ${Input({ name: "search", placeholder: "검색어를 입력하세요" })}
+  const { searchValue } = searchStore.get();
+  return `
+    <div
+      style="display: flex; flex-direction: row; align-items: center; gap: 8px;"
+    >
+      <div style="flex-grow: 1;">
+        ${Input({
+    name: "search",
+    placeholder: "검색어를 입력하세요",
+    value: searchValue
+  })}
+      </div>
+
+      ${Button({ name: "movie_search", content: "검색" })}
+    </div>
   `;
 };
+const handleSearch = () => {
+  const inputElement = document.querySelector("#search");
+  const searchValue = inputElement.value;
+  const hasSearchValue = !!searchValue;
+  searchParams.set("page", "1");
+  searchStore.set({ hasSearchValue, searchValue });
+};
+addEvent("keydown", "#search", (event) => {
+  if (event.key === "Enter") {
+    handleSearch();
+  }
+});
+addEvent("click", "#movie_search", () => {
+  handleSearch();
+});
 const SearchHeader = () => {
   return `
     <div style="display: flex; flex-direction: row; align-items: center;">
@@ -204,17 +254,21 @@ const SearchHeader = () => {
 };
 const ThumbnailHeader = () => {
   const { thumbnailTitle, thumbnailSrc, thumbnailVoteAverage } = thumbnailStore.get();
+  const { hasSearchValue } = searchStore.get();
   const rate = thumbnailVoteAverage.toFixed(1);
-  const backgroundImage = thumbnailSrc ? `url('https://image.tmdb.org/t/p/w1280${thumbnailSrc}');` : "";
+  const backgroundImage = hasSearchValue ? "none" : thumbnailSrc ? `url('https://image.tmdb.org/t/p/w1280${thumbnailSrc}');` : "";
+  const backgroundContainerClass = `background-container${hasSearchValue ? "__close" : ""}`;
+  const topRatedMovieClass = `top-rated-movie${hasSearchValue ? "__close" : ""}`;
   return `
     <header id="header-container">
-      <div class="background-container">
+      <div class="${backgroundContainerClass}">
         <div class="overlay" aria-hidden="true" style="background: no-repeat ${backgroundImage} background-size: cover;">
         </div>
+
         <div class="top-rated-container">
           ${SearchHeader()}
           
-          <div class="top-rated-movie">
+          <div class="${topRatedMovieClass}">
             <div class="rate">
               <img
                 src="${"/js-movie-review/"}assets/star_empty.png"
@@ -239,6 +293,7 @@ const render$2 = () => {
   oldContainer.replaceWith(newContainer);
 };
 thumbnailStore.subscribe(render$2);
+searchStore.subscribe(render$2);
 const EmptyMovie = () => {
   return `<div class="empty">
     <h4 style="font-size: 1.4rem; font-weight: 600;">
@@ -257,8 +312,9 @@ const ErrorMovie = (props) => {
   `;
 };
 const Skeleton = (props) => {
-  const _width = (props == null ? void 0 : props.width) ? `${props.width}px` : "100%";
-  const _height = (props == null ? void 0 : props.height) ? `${props.height}px` : "100%";
+  const { width, height } = props;
+  const _width = width ? `${width}px` : "100%";
+  const _height = height ? `${height}px` : "100%";
   return `
     <div 
       class="skeleton" 
@@ -300,6 +356,7 @@ const MovieItem = (props) => {
     </div>
   `;
 };
+const NOT_FOUND_ERROR = 22;
 const movieApi = (url, options) => {
   const _options = {
     ...options,
@@ -308,7 +365,7 @@ const movieApi = (url, options) => {
       accept: "application/json"
     }
   };
-  const _url = "https://api.themoviedb.org" + url;
+  const _url = `https://api.themoviedb.org${url}`;
   return fetch(_url, _options);
 };
 const getPopularMovie = async (params) => {
@@ -320,10 +377,27 @@ const getPopularMovie = async (params) => {
     );
     const data = await response.json();
     if (!response.ok) {
-      if (data.status_code === 22) {
+      if (data.status_code === NOT_FOUND_ERROR) {
         throw new Error("현재 페이지를 찾을 수 없습니다.");
+      } else {
+        throw new Error(
+          data.status_message || "영화 데이터를 불러오는데 실패했습니다."
+        );
       }
     }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+const getSearchedMovies = async (params) => {
+  const { page, query } = params;
+  try {
+    const response = await movieApi(
+      `/3/search/movie?query=${query}&include_adult=false&language=ko&region=kr&page=${page ?? 1}`,
+      { method: "GET" }
+    );
+    const data = await response.json();
     return data;
   } catch (error) {
     throw error;
@@ -343,22 +417,6 @@ const updateMovieThumbnail = (response) => {
     thumbnailSrc,
     thumbnailVoteAverage
   });
-};
-const searchParams = {
-  get: (key) => {
-    const params = new URLSearchParams(window.location.search);
-    return params.get(key);
-  },
-  set: (key, value, replace = false) => {
-    const params = new URLSearchParams(window.location.search);
-    params.set(key, value);
-    const newUrl = `${window.location.pathname}?${params.toString()}`;
-    if (replace) {
-      window.history.replaceState({}, "", newUrl);
-    } else {
-      window.history.pushState({}, "", newUrl);
-    }
-  }
 };
 const MAX_PAGE = 500;
 const DEFAULT_POPULAR_MOVIES = { results: [] };
@@ -400,6 +458,15 @@ const render$1 = ({ loader: loader2, isError, error }) => {
 };
 const loader = async () => {
   const page = Number(searchParams.get("page")) || 1;
+  const { hasSearchValue, searchValue } = searchStore.get();
+  if (hasSearchValue) {
+    const searchedMovies = await getSearchedMovies({
+      page,
+      query: searchValue
+    });
+    render$1({ isError: false, loader: searchedMovies });
+    return;
+  }
   try {
     const data = await getPopularMovie({ page });
     updateMovieThumbnail(data);
@@ -408,6 +475,7 @@ const loader = async () => {
     render$1({ isError: true, error: error.message });
   }
 };
+searchStore.subscribe(loader);
 addEvent("click", "#movie_more_load", () => {
   const page = Number(searchParams.get("page")) || 1;
   searchParams.set("page", page + 1);
@@ -415,6 +483,7 @@ addEvent("click", "#movie_more_load", () => {
 });
 loader();
 const App = () => {
+  initializeEventManager();
   return `
     <div id="wrap">
       ${ThumbnailHeader()}
@@ -423,7 +492,6 @@ const App = () => {
     </div>
   `;
 };
-initializeEventManager();
 const globalStore = createObserver({});
 console.log("npm run dev 명령어를 통해 영화 리뷰 미션을 시작하세요");
 console.log(
